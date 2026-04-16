e3185b7862ab24be47e0542e6cce061f901e6a39
framedstone <leeweixuan39@gmail.com>
2026-04-16 15:18:28 +0800

    feat(tools): external tools installation via ETInstallTools

--- ET.nvim/lua/ET/init.lua
+++ ET.nvim/lua/ET/init.lua
@@ -2,7 +2,19 @@ local M = {}
 
 -- Use for public setup via setup({})
 function M.setup(opts)
-	vim.notify('ET.nvim loaded!')
+	local bx = vim.fn.executable('bx')
+	local ctx7 = vim.fn.executable('ctx7')
+	if bx == 0 or ctx7 == 0 then
+		vim.notify(string.rep('=', 25) .. 'ET.nvim' .. string.rep('=', 25))
+		if bx == 0 then
+			vim.notify('- bx not installed (bravesearch cli)')
+		end
+		if ctx7 == 0 then
+			vim.notify('- ctx7 not installed (context7 cli)')
+		end
+		vim.notify('\n\nRun :ETInstallTools to install all external tools')
+		vim.notify(string.rep('=', 57))
+	end
 end
 
 return M
